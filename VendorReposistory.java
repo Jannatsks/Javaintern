b08f632b1a6156f6dbde693eb20882bbaab86de7
Jannatsks <126649350+Jannatsks@users.noreply.github.com>
2023-03-07 22:52:38 +0530

    Add files via upload

---
+++ VendorReposistory.java
@@ -0,0 +1,8 @@
+package com.example.demo.repo;
+
+import com.example.demo.model.Vendor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VendorReposistory extends JpaRepository <Vendor,Long>{
+
+}
