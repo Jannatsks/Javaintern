9fa6536c3a5a4262cc1a91c4b24db72dbec54ae8
Jannatsks <126649350+Jannatsks@users.noreply.github.com>
2023-03-07 23:08:03 +0530

    Delete VendorReposistory.java

--- VendorReposistory.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repo;
-
-import com.example.demo.model.Vendor;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VendorReposistory extends JpaRepository <Vendor,Long>{
-
-}
